510e9dc9ed38d91e3add2eb32548adec7036c7e1
Crowdbotics <team@crowdbotics.com>
2020-06-03 23:34:26 +0000

    add package blueprint-react-native-maps-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Maps59686', human_name: 'Maps', access_route: 'Maps59686', icon: 'map'},
 { name: 'Add-Item59682', human_name: 'Add-item', access_route: 'Add-Item59682'},
 { name: 'Maps59681', human_name: 'Maps', access_route: 'Maps59681', icon: 'map'},
 { name: 'Messenger', human_name: 'Messenger', access_route: 'Messenger', icon: 'comments'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Maps59686Navigator from '../features/Maps59686/navigator';
 import Add-Item59682Navigator from '../features/Add-Item59682/navigator';
 import Maps59681Navigator from '../features/Maps59681/navigator';
 import MessengerNavigator from '../features/Messenger/navigator';
@@ -23,6 +24,7 @@ const AppNavigator = {
       screen: SplashScreen
     },
     //@BlueprintNavigationInsertion
+Maps59686: { screen: Maps59686Navigator },
 Add-Item59682: { screen: Add-Item59682Navigator },
 Maps59681: { screen: Maps59681Navigator },
 Messenger: { screen: MessengerNavigator },
